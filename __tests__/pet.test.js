b08aea5099afdc679c4a464bf673f774554bf581
Abdu <abdu.gebril@gmail.com>
2022-07-02 19:20:21 +0100

    Added hunger and fitness properties

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -19,6 +19,19 @@ describe("constructor", () => {
         expect(pet.age).toEqual(0);
     });
 
+    it("has an initial hunger of 0", () => {
+        const pet = new Pet("Fido");
+
+        expect(pet.hunger).toEqual(0);
+    });
+
+    it("has an initial fitness of 10", () => {
+        const pet = new Pet("Fido");
+
+        expect(pet.fitness).toEqual(10);
+    });
+
+
 
 });
 
@@ -28,7 +41,7 @@ describe("growUp", () => {
 
         expect(pet.growUp).toBeInstanceOf(Function);
     });
-    
+
     it("increments the age by 1", () => {
         const pet = new Pet("Fido");
 
@@ -37,5 +50,22 @@ describe("growUp", () => {
         expect(pet.age).toEqual(1);
     });
 
+    it("increases the hunger by 5", () => {
+        const pet = new Pet("Fido");
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+
+    it("decreases fitness by 3", () => {
+        const pet = new Pet("Fido");
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
+    });
+
+    
 
 });
